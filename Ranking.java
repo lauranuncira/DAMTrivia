b383d78318e484adfa936222020ab9d4301ba02a
lauranuncira <laura.nuncira@iesjoandaustria.org>
2017-02-16 15:55:17 +0100

    Create Ranking.java

---
+++ Ranking.java
@@ -0,0 +1,14 @@
+package trivial.puntuacion;
+
+public class Ranking {
+	public static void main (String []args){
+		//
+		String jugador [] = new String [2];
+		Puntuacion (jugador);
+	}
+	public static void Puntuacion ( String jugador[] ){
+		
+		
+	}
+	
+}
